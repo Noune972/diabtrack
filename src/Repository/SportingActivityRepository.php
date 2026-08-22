b03e98d184a5a932be18be3d29917f2ab902b3f3
Noune972 <nathaliecamiul@yahoo.fr>
2026-08-22 17:22:34 +0200

    sporting activity

--- src/Repository/SportingActivityRepository.php
+++ src/Repository/SportingActivityRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\SportingActivity;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,28 +17,47 @@ class SportingActivityRepository extends ServiceEntityRepository
         parent::__construct($registry, SportingActivity::class);
     }
 
-    //    /**
-    //     * @return SportingActivity[] Returns an array of SportingActivity objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('s.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * Retourne les activités sportives d'un patient donné,
+     * triées de la plus récente à la plus ancienne (date puis heure).
+     *
+     * @return SportingActivity[]
+     */
+    public function findRecentesPourPatient(User $patient, int $limite = 30): array
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.patient = :patient')
+            ->setParameter('patient', $patient)
+            ->orderBy('s.date', 'DESC')
+            ->addOrderBy('s.hour', 'DESC')
+            ->setMaxResults($limite)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 
-    //    public function findOneBySomeField($value): ?SportingActivity
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+//    /**
+//     * @return SportingActivity[] Returns an array of SportingActivity objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('s.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?SportingActivity
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 }
